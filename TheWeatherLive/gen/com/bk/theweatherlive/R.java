1e90c9bed25f84620cc52948e347d28b27b6eecd
cheezus1 <bobsan_1996@abv.bg>
2014-02-26 12:29:12 +0200

    Adding buttons to the action bar

--- TheWeatherLive/gen/com/bk/theweatherlive/R.java
+++ TheWeatherLive/gen/com/bk/theweatherlive/R.java
@@ -28,6 +28,8 @@ public final class R {
         public static final int refresh_pressed=0x7f020004;
     }
     public static final class id {
+        public static final int action_about=0x7f080007;
+        public static final int action_help=0x7f080006;
         public static final int action_settings=0x7f080005;
         public static final int pager=0x7f080000;
         public static final int pager_title_strip=0x7f080001;
@@ -43,12 +45,14 @@ public final class R {
         public static final int weather_now=0x7f070000;
     }
     public static final class string {
+        public static final int action_about=0x7f050003;
+        public static final int action_help=0x7f050002;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int refresh_button=0x7f050005;
-        public static final int title_section1=0x7f050002;
-        public static final int title_section2=0x7f050003;
-        public static final int title_section3=0x7f050004;
+        public static final int refresh_button=0x7f050007;
+        public static final int title_section1=0x7f050004;
+        public static final int title_section2=0x7f050005;
+        public static final int title_section3=0x7f050006;
     }
     public static final class style {
         /** 
