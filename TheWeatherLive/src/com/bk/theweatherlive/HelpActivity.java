a9787035a38078960b5c21a11a5c3da3192d2eda
cheezus1 <bobsan_1996@abv.bg>
2014-02-27 19:33:33 +0200

    Pushing the source files together with the xml files (genius)

---
+++ TheWeatherLive/src/com/bk/theweatherlive/HelpActivity.java
@@ -0,0 +1,16 @@
+package com.bk.theweatherlive;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class HelpActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_help);
+	}
+}
