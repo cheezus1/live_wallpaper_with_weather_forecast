746c103b8a5588115855a5823ecb23c5369a13d3
cheezus1 <bobsan_1996@abv.bg>
2014-03-09 21:27:10 +0200

    Adding a random picture on the background

--- TheWeatherLive/gen/com/bk/theweatherlive/R.java
+++ TheWeatherLive/gen/com/bk/theweatherlive/R.java
@@ -29,11 +29,12 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_action_refresh=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int refresh=0x7f020002;
-        public static final int refresh_button=0x7f020003;
-        public static final int refresh_pressed=0x7f020004;
+        public static final int background=0x7f020000;
+        public static final int ic_action_refresh=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int refresh=0x7f020003;
+        public static final int refresh_button=0x7f020004;
+        public static final int refresh_pressed=0x7f020005;
     }
     public static final class id {
         public static final int action_about=0x7f0a0008;
