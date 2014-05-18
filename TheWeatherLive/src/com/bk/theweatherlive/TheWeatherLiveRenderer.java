0cc793bdf721f2ffc21817da53be1ea97b90bdda
kiril-kostadinov-tues <kiril.a.kostadinov@abv.bg>
2014-05-19 01:12:16 +0300

    Revert "App is now setting pure opengl retarded background"
    This reverts commit 13832fda96d0bd298364b3d98ac93adf1f0a0eab.

--- TheWeatherLive/src/com/bk/theweatherlive/TheWeatherLiveRenderer.java
+++ TheWeatherLive/src/com/bk/theweatherlive/TheWeatherLiveRenderer.java
@@ -2,72 +2,32 @@ package com.bk.theweatherlive;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
-import net.rbgrn.android.glwallpaperservice.GLWallpaperService;
-import android.hardware.SensorEvent;
-import android.opengl.GLU;
-import android.view.MotionEvent;
+
+import net.rbgrn.android.glwallpaperservice.*;
 
 public class TheWeatherLiveRenderer implements GLWallpaperService.Renderer {
 
-	private Circle circle;
-	private Triangle triangle;
-	
 	@Override
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+
+		gl.glClearColor(0.2f, 0.2f, 1f, 1f);
+		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
 		
-		gl.glEnable(GL10.GL_TEXTURE_2D);			//Enable Texture Mapping ( NEW )
-		gl.glShadeModel(GL10.GL_SMOOTH); 			//Enable Smooth Shading
-		gl.glClearColor(0.53f, 0.81f, 0.92f, 1.0f); 	//Black Background
-		gl.glClearDepthf(1.0f); 					//Depth Buffer Setup
-		gl.glEnable(GL10.GL_DEPTH_TEST); 			//Enables Depth Testing
-		gl.glDepthFunc(GL10.GL_LEQUAL); 			//The Type Of Depth Testing To Do
-		
-		//Really Nice Perspective Calculations
-		gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST); 
-		
-	}
-	
-	public void onSensorChanged(SensorEvent event) {
-	
 	}
 
-	public TheWeatherLiveRenderer() {
-		this.circle = new Circle();
-		this.triangle = new Triangle();
-	}
-		
 	@Override
 	public void onSurfaceChanged(GL10 gl, int width, int height) {
-		if (height == 0) {
-			height = 1;
-		}
+		// TODO Auto-generated method stub
 		
-		gl.glViewport(0, 0, width, height);
-		gl.glMatrixMode(GL10.GL_PROJECTION);
-		gl.glLoadIdentity();
-		
-		GLU.gluPerspective(gl, 45.0f, (float) width / (float) height, 0.1f, 100.0f);
-		
-		gl.glMatrixMode(GL10.GL_MODELVIEW);
-		gl.glLoadIdentity();
 	}
 
 	@Override
 	public void onDrawFrame(GL10 gl) {
-		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
-		gl.glLoadIdentity();
-		gl.glColor4f(1.0f, 1.0f, 0.0f, 1.0f);
-		gl.glTranslatef(1.7f, 3.0f, -12.5f);
-		circle.draw(gl);
+		// TODO Auto-generated method stub
 		
 	}
 
 	public void release() {
 		
-	}
-	
-	public boolean onTouchEvent(MotionEvent event) {
-		
-		return true;
 	}
 }
--- TheWeatherLive/src/com/bk/theweatherlive/TheWeatherLiveWallpaperService.java
+++ TheWeatherLive/src/com/bk/theweatherlive/TheWeatherLiveWallpaperService.java
@@ -1,12 +1,6 @@
 package com.bk.theweatherlive;
 
 import net.rbgrn.android.glwallpaperservice.GLWallpaperService;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.view.MotionEvent;
-import android.view.SurfaceHolder;
 
 public class TheWeatherLiveWallpaperService extends GLWallpaperService {
 
@@ -19,7 +13,7 @@ public class TheWeatherLiveWallpaperService extends GLWallpaperService {
 		return engine;
 	}
 	
-	class TheWeatherLiveEngine  extends GLEngine implements SensorEventListener {
+	class TheWeatherLiveEngine  extends GLEngine {
 		TheWeatherLiveRenderer renderer;
 		
 		public TheWeatherLiveEngine() {
@@ -30,46 +24,12 @@ public class TheWeatherLiveWallpaperService extends GLWallpaperService {
 		}
 		
 		public void onDestroy() {
-			
-			sensorManager.unregisterListener(this);
+			super.onDestroy();
 			
 			if(renderer != null) {
 				renderer.release();
 			}
 			renderer = null;
-			
-			setTouchEventsEnabled(false);
-			
-			super.onDestroy();
-		}
-		
-		private SensorManager sensorManager;
-		
-		@Override
-		public void onTouchEvent(MotionEvent event) {
-			super.onTouchEvent(event);
-			renderer.onTouchEvent(event);
-		}
-		
-		@Override
-		public void onCreate(SurfaceHolder surfaceHolder) {
-			super.onCreate(surfaceHolder);
-			
-			setTouchEventsEnabled(true);
-			
-			sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-			Sensor orientationSensor = sensorManager.getDefaultSensor(SensorManager.SENSOR_ORIENTATION);
-			sensorManager.registerListener(this, orientationSensor, SensorManager.SENSOR_DELAY_GAME);
-		}
-		
-		public void onSensorChanged(SensorEvent event) {
-			
-		}
-
-		@Override
-		public void onAccuracyChanged(Sensor sensor, int accuracy) {
-			// TODO Auto-generated method stub
-			
 		}
 	}
 	
